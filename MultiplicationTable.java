0ba8a411a1fce8da5c985b844f9dc782f9a91d9d
Jovilyn Esquerra <144090346+oceaanjj@users.noreply.github.com>
2024-12-28 19:10:39 +0800

    Create MultiplicationTable.java

---
+++ MultiplicationTable.java
@@ -0,0 +1,14 @@
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("MULTIPLICATION TABLE");
+        
+        for(int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++){
+                int multiply = i * j;
+                System.out.printf("| %4d |", multiply);
+            }
+            System.out.println();
+        }
+    }
+}
